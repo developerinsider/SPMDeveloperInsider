31df5ae6286005bd4b05c80891725ae359bcf8f9
Vineet Choudhary <vineetchoudhary@live.in>
2018-08-26 23:59:58 +0530

    changed to class

--- Sources/SPMDeveloperInsider/SPMDeveloperInsider.swift
+++ Sources/SPMDeveloperInsider/SPMDeveloperInsider.swift
@@ -1,6 +1,8 @@
 public struct SPMDeveloperInsider {
-    var name = "Developer Insider"
-
+    init() {
+        
+    }
+    
     public func Sum<T: Numeric>(num1: T, num2: T) -> T {
         return num1 + num2;
     }
