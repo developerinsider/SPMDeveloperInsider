85cac20ee547306950d69db52ada0ff65dadfd0f
Vineet Choudhary <vineetchoudhary@live.in>
2018-08-26 23:40:15 +0530

    Fixed access control and added sum func

--- Sources/SPMDeveloperInsider/SPMDeveloperInsider.swift
+++ Sources/SPMDeveloperInsider/SPMDeveloperInsider.swift
@@ -1,3 +1,7 @@
-struct SPMDeveloperInsider {
+public struct SPMDeveloperInsider {
     var name = "Developer Insider"
+
+    public func Sum<T>(T num1, T num2) -> T {
+        return num1 + num2;
+    }
 }
